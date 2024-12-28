642f73958c2fcdf3c52a801715661064362cc819
Thakur Saad <thakursaad613@gmail.com>
2024-12-28 14:23:20 +0600

    blog system

---
+++ src/app/module/blog/blog.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    subTitle: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    blog_image: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
