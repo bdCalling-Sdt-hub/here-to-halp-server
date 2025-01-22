772c924063bdc5d3acaf75028b702122fd56a997
ThakurSaad <thakursaad613@gmail.com>
2025-01-22 14:44:11 +0600

    routes and controller done

---
+++ src/app/module/job/job.routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const JobController = require("./Job.controller");
+const config = require("../../../config");
+const auth = require("../../middleware/auth");
+const router = express.Router();
+
+router
+  .post("post-job", auth(config.auth_level.admin), JobController.postJob)
+  .get("get-all-job", JobController.getAllJob)
+  .get("get-single-job", JobController.getSingleJob)
+  .delete(
+    "delete-single-job",
+    auth(config.auth_level.admin),
+    JobController.deleteSingleJob
+  );
+
+module.exports = router;
