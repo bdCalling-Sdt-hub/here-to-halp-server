fc91845d2f26835ccf12fb9c00773efe12105f4b
ThakurSaad <thakursaad613@gmail.com>
2025-01-22 12:38:27 +0600

    job module added

---
+++ src/app/module/job/Job.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const JobSchema = new mongoose.Schema(
+  {
+    jobRequisitionId: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+JobSchema.pre("save", async function (next) {
+  if (!this.jobRequisitionId) {
+    this.jobRequisitionId = this._id.toString().toUpperCase();
+  }
+});
+
+const Job = mongoose.model("Job", JobSchema);
+
+module.exports = Job;
