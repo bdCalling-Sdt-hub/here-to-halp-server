f7c948262cb8b5dedff204c0404e9b6bd74b0844
ThakurSaad <thakursaad613@gmail.com>
2025-02-08 15:53:42 +0600

    catchasync

--- src/app.js
+++ src/app.js
@@ -9,7 +9,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://10.0.60.44:3002", "http://10.0.60.44:3004"],
+    origin: [
+      "http://10.0.60.44:3002",
+      "http://10.0.60.44:3005",
+      "http://10.0.60.44:3004",
+    ],
     credentials: true,
   })
 );
